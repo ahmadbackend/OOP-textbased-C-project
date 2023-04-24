989153933e0567b3ce635c1874c9259e31e47428
Ahmad basher <ahmadbasherjsphp@gmail.com>
2023-04-24 21:40:13 +0300

    first few steps and functions

---
+++ documentation.cpp
@@ -0,0 +1,42 @@
+
+#include<iostream>
+void invalid(){
+ std::cout<<"invalid"<<std::endl;  
+};
+void help(){
+std::cout<<"your aim is to make money"<<std::endl;
+}; 
+void status(){
+std::cout<<"Market looks good"<<std::endl;
+};
+void makeBid(){
+std::cout<<"Make a bid"<<std::endl;
+};
+void makeOffer(){
+std::cout<<"Make an offer"<<std::endl;
+}; 
+void checkWallet(){
+std::cout<<"Your wallet is empty."<<std::endl;
+}; 
+void nextFrame(){
+std::cout<<"Going to next time frame"<<std::endl;
+};
+void instructions(){
+    //1 print help
+std::cout<<"1: print help"<<std::endl;
+
+//2 print exchange stats
+std::cout<<"2: print exchange stats"<<std::endl;
+//3 make an offer=sell
+std::cout<<"3: Make an offer"<<std::endl;
+//make a bid =buy
+std::cout<<"4: Make a bid "<<std::endl;
+//5 print wallet
+std::cout<<"5: print wallet"<<std::endl;
+
+//6 continue = next time step
+std::cout<<"6: Continue"<<std::endl;
+
+std::cout<<"==================="<<std::endl;
+std::cout<<"Type 1-6, Type 0 to break"<<std::endl;
+}
